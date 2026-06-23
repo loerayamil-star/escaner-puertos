c837c9695ae10265ccf2102bdcf823858dc2e1dd
Yamil De Loera <loerayamil@gmail.com>
2026-06-23 16:14:08 -0600

    fix: corregir errores

--- escaner_puertos.py
+++ escaner_puertos.py
@@ -154,9 +154,9 @@ class EscanerPuertos:
                 )
             )
 # ── PUNTO DE ENTRADA
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = EscanerPuertos(root)
     root.mainloop()
-
-
